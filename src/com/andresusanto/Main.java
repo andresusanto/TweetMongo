ab0fa08cd4bfc1635ef9d6ecd77e44d74684b731
Andre Susanto <andre@andresusanto.info>
2015-11-22 20:41:17 +0700

    add init project

--- src/com/andresusanto/Main.java
+++ src/com/andresusanto/Main.java
@@ -1,8 +1,16 @@
 package com.andresusanto;
 
+import org.bson.Document;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+
 public class Main {
 
     public static void main(String[] args) {
-	    
+        MongoClient client = new MongoClient(new MongoClientURI("mongodb://167.205.35.19:27017,167.205.35.20:27017,167.205.35.21:27017,167.205.35.22:27017/?replicaSet=rs0"));
+        MongoDatabase db = client.getDatabase("andresusanto");
+
+        db.getCollection("coba").insertOne(new Document().append("hebat", "sekali").append("keren", "oke"));
     }
 }
